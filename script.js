d1e23b360c4bb5d7038864af7f25f1ba24fe210c
Konashkov <79665788+Konashkov@users.noreply.github.com>
2022-01-24 19:32:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+
+let number1 = Number(prompt("Enter your number 1"))
+let number2 = Number(prompt("Enter your number 2"))
+let thisArray = [number1+number2,number1-number2,number1*number2,number1/number2]
+console.log(thisArray)
