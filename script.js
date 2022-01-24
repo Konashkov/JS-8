d817627cc6abf8dcc7a2121bb07b98786ddf9e4c
Konashkov <79665788+Konashkov@users.noreply.github.com>
2022-01-24 19:33:40 +0200

    v.1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 let number1 = Number(prompt("Enter your number 1"))
 let number2 = Number(prompt("Enter your number 2"))
-let thisArray = [number1+number2,number1-number2,number1*number2,number1/number2]
+let thisArray = [number1+number2,number1-number2,number1/number2,number1*number2]
 console.log(thisArray)
